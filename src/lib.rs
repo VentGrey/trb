af5517134f3f00c4bb2de060629d0aab9306298c
VentGrey <omarpurataf@gmail.com>
2019-01-17 01:12:04 -0600

    added extern crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//Macros
+#[macro_use]
+extern crate gfx;
+extern crate gfx_text;
+extern crate gfx_window_glutin;
+extern crate glutin;
+
+// Módulos locales
 pub mod css;
 pub mod dom;
 pub mod html_parse;
